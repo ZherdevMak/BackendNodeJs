26d45eb7b386f4322cc21c611813faa2407423f4
ZherdevMak <Zherdev.mak@gmail.com>
2022-10-15 19:35:46 +0300

    host added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT | 7500
 const users = require('./users-Router')
 const cors = require('cors')
 const bodyPsrser = require('body-parser')
+const host = '0.0.0.0';
 
 app.use(bodyPsrser.urlencoded({extended: false}))
 app.use(bodyPsrser.json())
@@ -16,7 +17,7 @@ app.get('/', async (req, res) => {
 app.use((req, res) => {
     res.send(404)
 })
-app.listen(port, () => {
+app.listen(port, host, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
